621c0d83e3f58dfd581ed324e87ec124c01246a0
matyas-ww <matyas.chovan@seznam.cz>
2024-10-06 14:02:42 +0200

    hotový program

--- first.py
+++ first.py
@@ -1,3 +1,12 @@
-# passmmsm
-pass
-print("iu")
+
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        return print(f"Číslo {cislo} je sudé")
+    else:
+        return print(f"Číslo {cislo} je liché")
+
+
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000)
+sudy_nebo_lichy(70)
+sudy_nebo_lichy(51)
